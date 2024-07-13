99d6935036018993f9bf9306305bc112388ff512
Alisher <nebulous4707@gmail.com>
2024-07-13 23:22:55 +0500

    added config

--- services/auth/internal/config/config.go
+++ services/auth/internal/config/config.go
@@ -1,13 +1,20 @@
 package config
 
-const port = 55051
+import "github.com/caarlos0/env/v11"
 
 // Config holds configuration settings for the application.
 type Config struct {
-	GRPCServerPort int
+	GRPCServerPort int    `env:"GRPC_SERVER_PORT,required"`
+	DSN            string `env:"DB_DSN,required"`
 }
 
 // Load attempts to load the application configuration.
 func Load() (*Config, error) {
-	return &Config{GRPCServerPort: port}, nil
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
 }
